916323939c44451c804fd263ba9b6e9c1ad1ecab
Sean Petrie <sean.petrie17@gmail.com>
2021-07-30 16:32:05 -0500

    iniital commit

---
+++ DifficultyTweaks/Main.cs
@@ -0,0 +1,76 @@
+﻿using HarmonyLib;
+using Kingmaker.Settings;
+using Kingmaker.Settings.Difficulty;
+using System;
+using UnityEngine;
+using UnityModManagerNet;
+using static UnityModManagerNet.UnityModManager;
+
+namespace DifficultyTweaks {
+    static class Main {
+
+        public static Settings Settings;
+        public static bool Enabled;
+        public static ModEntry Mod;
+        [System.Diagnostics.Conditional("DEBUG")]
+        public static void Log(string msg) {
+            Mod.Logger.Log(msg);
+        }
+
+        static bool Load(UnityModManager.ModEntry modEntry) {
+            var harmony = new Harmony(modEntry.Info.Id);
+            harmony.PatchAll();
+            Mod = modEntry;
+            Settings = Settings.Load<Settings>(modEntry);
+            modEntry.OnGUI = OnGUI;
+            modEntry.OnSaveGUI = OnSaveGUI;
+
+            return true;
+        }
+
+        static bool OnToggle(UnityModManager.ModEntry modEntry, bool value) {
+            Enabled = value;
+            Settings.enableOverride = !Settings.enableOverride;
+            return true;
+        }
+
+        static void OnGUI(UnityModManager.ModEntry modEntry) {
+            GUILayout.BeginVertical();
+            GUILayout.BeginHorizontal();
+            if (GUILayout.Button("Disable", GUILayout.ExpandWidth(false))) {
+                Settings.enableOverride = false;
+            }
+            GUILayout.Label(
+                string.Format("Difficulty Override: {0}", Settings.enableOverride ? Settings.difficultySetting.ToString() : "Disabled"), GUILayout.ExpandWidth(false)
+            );
+            GUILayout.EndHorizontal();
+            GUILayout.BeginHorizontal();
+            foreach (GameDifficultyOption difficulty in Enum.GetValues(typeof(GameDifficultyOption))) {
+                if (GUILayout.Button(difficulty.ToString(), GUILayout.ExpandWidth(false))) {
+                    Settings.difficultySetting = difficulty;
+                    Settings.enableOverride = true;
+                }
+            }
+            GUILayout.EndHorizontal();
+            GUILayout.EndVertical();
+        }
+
+        static void OnSaveGUI(UnityModManager.ModEntry modEntry) {
+            Settings.Save(modEntry);
+        }
+
+        [HarmonyPatch(typeof(DifficultyPresetsController), "CurrentDifficultyCompareTo", new Type[] { typeof(GameDifficultyOption) })]
+        static class Difficulty_Override_Patch {
+            static void Postfix(GameDifficultyOption gameDifficultyOption, ref int __result) {
+                if (!Settings.enableOverride) { return; }
+                if (gameDifficultyOption < Settings.difficultySetting) {
+                    __result = 1; 
+                } else if(gameDifficultyOption == Settings.difficultySetting) {
+                    __result = 0;
+                } else if (gameDifficultyOption > Settings.difficultySetting) {
+                    __result = -1;
+                }
+            }
+        }
+    }
+}
---
+++ DifficultyTweaks/Settings.cs
@@ -0,0 +1,13 @@
+﻿using Kingmaker.Settings;
+using UnityModManagerNet;
+
+namespace DifficultyTweaks {
+    public class Settings: UnityModManager.ModSettings {
+        public GameDifficultyOption difficultySetting = GameDifficultyOption.Custom;
+        public bool enableOverride = false;
+
+        public override void Save(UnityModManager.ModEntry modEntry) {
+            Save(this, modEntry);
+        }
+    }
+}
